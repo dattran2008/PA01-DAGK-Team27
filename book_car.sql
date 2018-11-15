daf35d7b66fc11ad84d59eb0527b73d304aecc1e
Pham Nguyen Truong Nguyen <pntrnguyen@gmail.com>
2018-11-15 23:59:43 +0700

    Add files via upload

--- book_car.sql
+++ book_car.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 -- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: book_car
@@ -24,14 +23,14 @@ DROP TABLE IF EXISTS `customer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `customer` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `phone_number` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `address` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `note` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `note` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `state` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +39,7 @@ CREATE TABLE `customer` (
 
 LOCK TABLES `customer` WRITE;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (1,'name1','0123456789','10 Nguyen hue, Quan 1, TPHCM','Pho di bo','no'),(2,'name1','0345476679','135 Nguyen Van Cu, Quan 5, TPHCM','Khoa hoc tu nhien','no'),(3,'name1','0345476679','135 Nguyen Van Cu, Quan 5, TPHCM','Khoa hoc tu nhien','no'),(4,'name1','0345476679','135 Nguyen Van Cu, Quan 5, TPHCM','Khoa hoc tu nhien','no'),(5,'name1','0345476679','135 Nguyen Van Cu, Quan 5, TPHCM','Khoa hoc tu nhien','no'),(6,'name1','0345476679','135 Nguyen Van Cu, Quan 5, TPHCM','Khoa hoc tu nhien','no'),(7,'name1','0345476679','135 Nguyen Van Cu, Quan 5, TPHCM','Khoa hoc tu nhien','no'),(8,'name1','0345476679','135 Nguyen Van Cu, Quan 5, TPHCM','Khoa hoc tu nhien','no');
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -67,85 +67,53 @@ LOCK TABLES `driver` WRITE;
 /*!40000 ALTER TABLE `driver` DISABLE KEYS */;
 /*!40000 ALTER TABLE `driver` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-11-04 22:28:45
-=======
--- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
---
--- Host: localhost    Database: book_car
--- ------------------------------------------------------
--- Server version	8.0.12
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `customer`
+-- Table structure for table `employee`
 --
 
-DROP TABLE IF EXISTS `customer`;
+DROP TABLE IF EXISTS `employee`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `customer` (
-  `id` int(11) NOT NULL,
-  `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `phone_number` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `address` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `note` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `state` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `customer`
+-- Dumping data for table `employee`
 --
 
-LOCK TABLES `customer` WRITE;
-/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `driver`
+-- Table structure for table `refreshtoken`
 --
 
-DROP TABLE IF EXISTS `driver`;
+DROP TABLE IF EXISTS `refreshtoken`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `driver` (
+CREATE TABLE `refreshtoken` (
   `id` int(11) NOT NULL,
-  `user` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `state` varchar(10) NOT NULL,
+  `rfToken` varchar(500) DEFAULT NULL,
+  `rdt` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `driver`
+-- Dumping data for table `refreshtoken`
 --
 
-LOCK TABLES `driver` WRITE;
-/*!40000 ALTER TABLE `driver` DISABLE KEYS */;
-/*!40000 ALTER TABLE `driver` ENABLE KEYS */;
+LOCK TABLES `refreshtoken` WRITE;
+/*!40000 ALTER TABLE `refreshtoken` DISABLE KEYS */;
+/*!40000 ALTER TABLE `refreshtoken` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -157,5 +125,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-04 22:28:45
->>>>>>> 968546c079fe7ff59fd3d6f480c9452753b81d1c
+-- Dump completed on 2018-11-15 23:59:08
